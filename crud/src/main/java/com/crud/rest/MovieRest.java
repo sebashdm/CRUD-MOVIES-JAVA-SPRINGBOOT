14194b2e0372b7a5b49b69a48d2127976c6a1822
sebashdm <sebashdm@gmail.com>
2020-08-26 12:26:20 -0500

    feat: Se crea la clase movierest se hace la inyeccion de metodos del reposotorio y se crea la funcionalidad del crud, post,put,update,delete

---
+++ crud/src/main/java/com/crud/rest/MovieRest.java
@@ -0,0 +1,36 @@
+package com.crud.rest;
+import com.crud.dao.MovieRepository;
+import com.crud.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "movies")
+public class MovieRest {
+
+    @Autowired
+    private MovieRepository repository;
+
+    @PostMapping(value = "/save")
+    public void save(@RequestBody Movie movie){
+        repository.save(movie);
+    }
+
+    @GetMapping(value = "/list")
+    public Iterable<Movie> list(){
+        return repository.findAll();
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public void delete(@PathVariable("id") Long id){
+        repository.deleteById(id);
+    }
+
+    @PutMapping (value = "/update")
+    public Movie update(@RequestBody Movie movie){
+        if(movie.getId() != null){
+            return repository.save(movie);
+        }
+        throw  new RuntimeException("El id no es valido");
+    }
+}
