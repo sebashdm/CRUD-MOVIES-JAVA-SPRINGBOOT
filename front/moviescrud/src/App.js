3ac9b5368a6d1bf44a178dc1ef46d08673ee43e6
sebashdm <sebashdm@gmail.com>
2020-08-27 16:40:56 -0500

    fix: se corrige estructura de la tabla

--- front/moviescrud/src/App.js
+++ front/moviescrud/src/App.js
@@ -24,10 +24,10 @@ export default class App extends Component{
       <thead>
         <tr>
           <td>Id</td>
-          <td>Nombre Juego</td>
-          <td>Consola</td>
-          <td>Valor</td>
-          <td>¿Disponible?</td>
+          <td>Movie Name</td>
+          <td>Producer</td>
+          <td>Year</td>
+          <td>Actions</td>
         </tr>
       </thead>
       <tbody>
@@ -35,8 +35,11 @@ export default class App extends Component{
         {this.state.movies.map((movie)=>{
           return(
             <tr key={movie.id}>
+            <td>{movie.id}</td>
             <td>{movie.name}</td>
-            <td>{movie.console}</td>
+            <td>{movie.producer}</td>
+            <td>{movie.year}</td>
+            <td></td>
           </tr>
           )
         })}
